fbd3af5ce7b4a812e97b1a8e1afb9c93868a466a
DIEGO ERICK <DIEGO ERICK@LAPTOP-1BPHSF7N>
2023-11-29 09:40:05 -0600

    Agregar archivos de proyecto.

---
+++ VALORES ECUACIONES/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace VALORES_ECUACIONES
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(146, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Z= X² + X ³";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(45, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 30);
+            label2.TabIndex = 1;
+            label2.Text = "X";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(146, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 30);
+            label3.TabIndex = 2;
+            label3.Text = "X²";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(235, 73);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 30);
+            label4.TabIndex = 3;
+            label4.Text = "X³";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(337, 73);
+            label5.Name = "label5";
+            label5.Size = new Size(26, 30);
+            label5.TabIndex = 4;
+            label5.Text = "Z";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 113);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(92, 109);
+            listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(110, 113);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(92, 109);
+            listBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(208, 113);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(92, 109);
+            listBox3.TabIndex = 7;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 15;
+            listBox4.Location = new Point(306, 113);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(92, 109);
+            listBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(137, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 23);
+            button1.TabIndex = 9;
+            button1.Text = "CALCULAR VALORES";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 64, 64);
+            ClientSize = new Size(412, 294);
+            Controls.Add(button1);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "VALORES ECUACIONES";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Button button1;
+    }
+}
